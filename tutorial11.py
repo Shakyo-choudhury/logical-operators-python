18158d98d104465840f6e308fe00367e8b97f493
Shakyo choudhury <94360466+Shakyo-choudhury@users.noreply.github.com>
2023-07-15 21:38:09 +0530

    Add files via upload

---
+++ tutorial11.py
@@ -0,0 +1,16 @@
+A = True
+B = False
+
+result = A and B
+print(result)  # Output: False
+
+A = True
+B = False
+
+result = A or B
+print(result)  # Output: True
+
+A = True
+
+result = not A
+print(result)  # Output: False
